38f2b91d363d4f2c7f6cb1bcde5a458570cecf84
Cees van Kemenade <cde@vinzi.nl>
2013-03-14 20:55:40 +0100

    Added vParseName including unit-tests.

---
+++ vinzi.tools/src/main/clojure/vinzi/tools/vNameParse.clj
@@ -0,0 +1,70 @@
+(ns vinzi.tools.vNameParse
+   (:use	[clojure [pprint :only [pprint pp]]]
+        [clojure [stacktrace :only [print-stack-trace root-cause]]]
+        [clojure.tools [logging :only [error info trace debug warn]]])
+   (:require [clojure.string :as str]))
+
+
+(def infix-mask
+  ;; greed match, so order prefixes in decreasing number of parts (and from long to shorter)
+  ;; a function is applied over the list to replace "van den"  with " van\s*den " (to match one or more spaces between infix parts
+  ;; and add the surrounding spaces and to match first and last parts of the name
+  ((fn [infs]
+     (let [make-mask (fn [inf]
+                       (re-pattern (str "(?iu)^([\\w\\s\\-\\.]*) (" (str/replace inf #"\s+" "\\\\s+") ") ([\\w\\s\\-]*)$"))) ]
+     (map make-mask infs)))
+    ["v d"
+     "van der"
+     "van den"
+     "van de"
+     "van"
+     "de la"
+     "de"
+     "la"
+     "le"
+     "in het"
+     "in 't"
+     "vd"
+     "vden"]))
+  
+(defn split-name 
+  "Split a name in :firstnames, :infix and :lastnames. If there is no infix we split on whitespace and assume the last part
+   is the last-names (separated by dashes -)."
+  [s]
+  (debug "(split-name): processing string: " s)
+  (if-let [splitted (some identity (map #(re-find % s) infix-mask))]
+    (let [[_ firstnames infix lastnames] splitted]
+      {:firstnames firstnames
+       :infix      infix
+       :lastnames  lastnames})
+    (let [splitted (str/split s #"\s+") ]
+      {:firstnames (str/join " " (drop-last splitted))
+       :infix   ""
+       :lastnames (last splitted)})
+    ))
+
+(defn add-initials-abbrev 
+  "Add the initials and abbreviation to a record returned by split-name. 
+      The abbreviation consist of first letters of firstname (upcase), infix (lowcase) and lastnames (upcase)." 
+  [rec]
+  (debug "(add-initials-abbrev): processing record: " rec)
+  (let [initials (if (seq (:firstnames rec)) 
+                   (-> (:firstnames rec)
+                   (str/split #"\s+" )
+                   ((partial map #(str/upper-case (first %)))))
+                   ())
+        inf   (if (seq (:infix rec))
+                (-> (:infix rec)
+                  (str/split #"\s+")
+;;                  ((partial (map #(str/replace % #"'t" "het"))))
+                  ((partial map #(str/lower-case (first %))))
+                  ((partial remove #(= % "'"))))
+                "")
+        lst (-> (:lastnames rec)
+                   (str/split #"[\-\s]+" )
+                   ((partial map #(str/upper-case (first %)))))
+        abbrev (apply str (concat initials inf lst))
+        initials (-> initials
+                   (interleave (repeat "."))
+                   ((partial apply str)))]
+    (assoc rec :initials initials  :abbrev abbrev)))
---
+++ vinzi.tools/src/test/clojure/vinzi/tools/test/vNameParse.clj
@@ -0,0 +1,25 @@
+(ns vinzi.tools.test.vNameParse
+  (:use clojure.test)
+  (:require
+    [clojure.string :as str]
+    [vinzi.tools
+     [vDateTime :as vDate]
+     [vNameParse :as vNP]
+     [vMap  :as vMap]]))
+
+(deftest name-parse
+  (are [inp outp] (vMap/map-compare (vNP/add-initials-abbrev (vNP/split-name inp)) outp)
+       "jan van abc"  {:firstnames "jan" :infix "van" :lastnames "abc" :initials "J." :abbrev "JvA"}
+       "Jan van abc"  {:firstnames "Jan" :infix "van" :lastnames "abc" :initials "J." :abbrev "JvA"}
+       "Jan van Abc"  {:firstnames "Jan" :infix "van" :lastnames "Abc" :initials "J." :abbrev "JvA"}
+       "jan a"  {:firstnames "jan" :infix "" :lastnames "a" :initials "J."  :abbrev "JA"}
+       "Jan van de Abc"  {:firstnames "Jan" :infix "van de" :lastnames "Abc" :initials "J." :abbrev "JvdA"}
+       "Jan Van De Abc"  {:firstnames "Jan" :infix "Van De" :lastnames "Abc" :initials "J." :abbrev "JvdA"}
+       "Jan in 't Abc"  {:firstnames "Jan" :infix "in 't" :lastnames "Abc" :initials "J." :abbrev "JiA"}
+       "Jan Piet in 't Abc"  {:firstnames "Jan Piet" :infix "in 't" :lastnames "Abc" :initials "J.P." :abbrev "JPiA"}
+       "Jan Piet Abc"  {:firstnames "Jan Piet" :infix "" :lastnames "Abc" :initials "J.P." :abbrev "JPA"}
+       "Jan Van De Abc-Def"  {:firstnames "Jan" :infix "Van De" :lastnames "Abc-Def" :initials "J." :abbrev "JvdAD"}
+       "Jan Van De Abc Def"  {:firstnames "Jan" :infix "Van De" :lastnames "Abc Def" :initials "J." :abbrev "JvdAD"}
+       )
+  )
+
