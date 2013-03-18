8c422e888eae5f854708130eaa1e58c8a3540857
Cees van Kemenade <cde@vinzi.nl>
2013-03-18 14:50:53 +0100

    Moved reuseable code from vinzi.olap4Clj (vinzi.cdp) to vinzi.tools (parameters-replacement, type-conversion and vCacheXml).

---
+++ vinzi.tools/src/main/clojure/vinzi/tools/vCacheXml.clj
@@ -0,0 +1,100 @@
+(ns vinzi.tools.vCacheXml
+  (:use	[clojure pprint]
+        [clojure.tools logging])
+  (:require [clojure
+             [xml :as xml]
+             [string :as str]]
+            [vinzi.data.zip :as vZip]
+            [vinzi.tools
+             [vFile :as vFile]
+             [vExcept :as vExcept]]))
+
+
+(defn create-xml-cache 
+  [xml-init xml-clear]
+  (let [xmlFileCache (atom {})]
+    (letfn [(extend-xml-filepath 
+              ;;Path should be extended. When used as a standalone application
+              ;;this function extends to the current directory. 
+              ;;NOTE: when used as a pentaho-plugin the plugin adaptor should take care that fName
+              ;;contains a full path relative to root (no guarantees about the current folder). 
+              [fName]
+              (if (vFile/full-path? fName)
+                fName
+                (vFile/filename (vFile/get-current-dir) fName)))
+            (read-xml-file  
+              ;;Read a xml-file and compile it's code section (if present).
+              [fName]
+              (let [lpf "(read-xml-file): "
+                    read-xml (fn [fName]
+                               (try 
+                                 (xml/parse fName)
+                                 (catch Exception e
+                                   (let [prefix (str lpf "Could NOT open xml-file: " fName)
+                                         logMsg (str prefix "\nException: " (.getMessage e))]
+                                     (error logMsg)
+                                     (binding [*out* *err*]
+                           (println prefix)) ;; file-info (duplic?)
+                                     (throw e)))))]  ;; re-throw same exception
+                (let [xml  (read-xml fName)]
+                  (when (fn? xml-init)
+                    (xml-init fName xml))
+                  xml)))
+            (get-xml-file 
+              ;; Try to read the config-file from the request from the internal cache. If it is not available read it from disc and cache it.
+              [fName]
+              (let [lpf "(get-xml-file): "
+                    fName (extend-xml-filepath fName)
+                    ;; key should be based on the full-filename
+                    keyfName (keyword fName)]
+                (if-let [cfg (keyfName @xmlFileCache)]
+                  (do
+                    (trace "retrieved configuration " fName " from cache")
+                    cfg)  ;; file was already in cache
+                  (if-let [cfg (read-xml-file fName)]
+                    (let [topLevelText (remove map? (:content cfg))]
+                      (when (seq topLevelText)
+                        (vExcept/throw-except lpf "Top-level of xml contains text (tags missing?): " (str/join "\n" topLevelText)))
+                      (trace "Loaded a new configuration from file: " fName
+                             ;;                  "\nContents: " (with-out-str (pprint cfg))
+                             "\nAdding contents to the internal xml-cache.")
+                      (swap! xmlFileCache (fn [cc] (assoc cc keyfName cfg)))
+                      cfg)
+                    (error lpf "Could not read " fName " (no valid xml?)")))))
+            (list-xml-file-entries []
+                              (keys @xmlFileCache))
+            (update-xml-entry 
+              ;;Update a xml-entry in memory to store additional information or modifications.
+              ;;NOTE: the version on file is not updated.
+              [fName newEntry]
+              {:pre [(not (vZip/is-zipper? newEntry))]} ;; in memory we store xml.
+              (let [lpf "(update-xml-entry): "
+                    _   (trace lpf "the fName to be keywordized is: " fName)
+                    keyfName (-> fName
+                               (extend-xml-filepath)
+                               (keyword))]
+                (trace lpf "The cache-entry is: " (keyfName @xmlFileCache))
+                (if-let [current (keyfName @xmlFileCache)]
+                  (swap! xmlFileCache (fn [cc] (assoc cc keyfName newEntry)))
+                  (vExcept/throw-except lpf "Entry with name " keyfName "does not exist (No update performed),"
+                                        "\n\t Valid file-entries are: " (str/join "\n\t\t" (list-xml-file-entries)) "\n"))))
+            (clear-xml-cache 
+              ;; Clear the xml-cache.
+              []
+              (info "(clear-cache): Clearing the xml-file cache")
+              ;; TODO: add exception handling
+              (let [xmls @xmlFileCache]
+                (doseq [xml (vals xmls)]
+                  (when (fn? xml-clear)
+                    (xml-clear xml))
+             ))
+              (swap! xmlFileCache (fn [_] {}))
+              ;;{:plain "succes: xml-cache cleared"}
+              (str "<html><h2>xml/clear-cache</h2><p>The xml-cache was cleared at " (java.util.Date.) "</p></html>"))]
+      {:get-xml-file      get-xml-file
+       :update-xml-entry  update-xml-entry
+       :clear-xml-cache   clear-xml-cache
+       :list-xml-file-entries list-xml-file-entries
+       }
+            
+            )))	    
---
+++ vinzi.tools/src/main/clojure/vinzi/tools/vString.clj
@@ -0,0 +1,116 @@
+(ns vinzi.tools.vString
+  (:use	[clojure [pprint :only [pprint pp]]]
+         [clojure [stacktrace :only [print-stack-trace root-cause]]]
+	   [clojure.tools [logging :only [error info trace debug warn]]])
+  (:require [clojure
+	     [string :as str]]
+	    [vinzi.tools
+         [vDateTime :as vDate]
+         [vExcept :as vExcept]]))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  functions for handeling string-parameters
+;;    1. Replace parameters of shape ${  } in a string
+;;    2. Do type conversion on strings according to a type-map  
+;;       (special cases such as null, and NaN values handled too)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn replace-params "Takes 'code' (sequence of strings) and 
+ performse the parameter substitution as defined in 'parmams' (keywords translated to strings).
+ The result is a map containing :code (as a string) and 
+ :replCnt (a map showing the number of replacements.)" 
+  [code params]
+  (letfn [(replace-par
+            [{:keys [code replCnt]} [k v]]
+            (let [lpf "(replace-par): "]
+              (if (and code k (not (nil? v)))
+                (let [pat (re-pattern (str "\\$\\{" (name k) "\\}"))
+                      cnt (count (re-seq pat code))]
+                  (when (replCnt k)
+                    ;; TODO: replace with one-liner vExcept/throw-except
+                    (let [msg (str lpf "The parameter " k " is defined more than once")]
+                      (error msg)
+                      (throw (Exception. msg))))
+                  (trace lpf "execute replace " pat " --> " v
+                         " in query: " code " (" cnt " replacements)")
+                  (let [res 
+                  {:code (str/replace code pat (str v))
+                   :replCnt (assoc replCnt k cnt)}]
+                    ;;(println "result of replacement: " res)
+                    res))
+                ;; TODO: replace with one-liner vExcept/throw-except
+                (let [msg (str  lpf "missing param (template, replace-key, replace-val, other-params)"
+                                "\n\ttemplate=" code
+                                "\n\treplace-key=" k
+                                "\n\treplace-val=" v
+                                "\n\tother-params=" (dissoc params k))]
+                  (error msg)
+                  (throw (Exception. msg))))))]
+         (let [lpf "(replace-pars): "
+               cumm {:code code :replCnt {} }
+               cumm (reduce replace-par cumm (seq params))]
+;;           (trace lpf "after replacement: " (:code cumm))
+           cumm)))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;    Do type conversion on strings according to a type-map  
+;;       (special cases such as null, and NaN values handled too)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn convert-type-params 
+  "For all parameters defined in the type-map convert their type to the corresponding type.  
+   If no type is defined the parameters are assumed to be string-parameters (not checked)."
+  [pars typeMap]
+  (let [lpf "(convert-type-params): "
+        convert-int  (fn [x]
+                       (when x
+                         (let [x (str/trim x)]
+                           (when (not (re-find #"(?i)null" x))   ;; null will be mapped to a nil-value
+                             (Long/parseLong x)))))
+        convert-double (fn [x]
+                         (when x
+                           (let [x (str/trim x)]
+                             (when (not (re-find #"(?i)null" x))   ;; null will be mapped to a nil-value
+                               (if (re-find #"(?i)nan" x)
+                                 Double/NaN
+                                 (Double/parseDouble x))))))
+        convert-date  (fn [x]
+                         (when x
+                           (let [x (str/trim x)]
+                             (when (not (re-find #"(?i)null" x))   ;; null will be mapped to a nil-value
+                               (vDate/convert-to-date x)))))
+        convert-timestamp  (fn [x]
+                             (when x
+                               (let [x (str/trim x)]
+                                 (when (not (re-find #"(?i)null" x))   ;; null will be mapped to a nil-value
+                                   (vDate/convert-to-timestamp x)))))
+        convert-boolean  (fn [x]
+                             (when x
+                               (let [x (str/upper-case (str/trim x))]
+                                 (if (= x "TRUE")
+                                   true
+                                   (if (= x "FALSE")
+                                     false
+                                     (vExcept/throw-except lpf "Boolean should either be TRUE or FALSE. Received value: " x))))))
+        convert-type (fn [cumm [k tp]]
+                       (if-let [value (get cumm k)]
+                         (let [converted (case tp 
+                                           (:int :integer :long) (convert-int value)
+                                           (:double :real) (convert-double value)
+                                           :date (convert-date value)
+                                           :timestamp (convert-timestamp value)
+                                           :boolean   (convert-boolean value)
+                                           (vExcept/throw-except lpf "Unknown type: " tp 
+                                                                 " in type-map: " typeMap 
+                                                                 " for parameters: " pars))]
+                           (assoc cumm k converted))
+                         cumm))]
+  (reduce convert-type pars typeMap)))
+
