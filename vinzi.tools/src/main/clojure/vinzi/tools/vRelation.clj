279155c7037d238c05bfb28002543c341feba0ab
Cees van Kemenade <cde@vinzi.nl>
2014-01-22 16:18:04 +0100

    Added four join-functions that join multiple sets or lists
    Each function can take an arbitrary number of xrel (lists or sets)
    - inner-join
    - left-outer
    - right-outer
    - full-outer

--- vinzi.tools/src/main/clojure/vinzi/tools/vRelation.clj
+++ vinzi.tools/src/main/clojure/vinzi/tools/vRelation.clj
@@ -6,6 +6,7 @@
               [string :as str]
               [set :as set]]
              [vinzi.tools
+              [vExcept :as vExcept]
               [vMap :as vMap]]))
 
 
@@ -32,11 +33,84 @@
        (map split-rec-aux recs)))   
 
 
-;(defn join-recs
-;  "Join the relations 'xrel' and 'yrel' on 'kKeys' and store the other keys under 'xOther' and 'yOther'."
-;  ([xrel yrel kKeys] (join-recs xrel yrel kKeys :xOther :yOther))
-;  ([xrel yrel kKeys]
-;  (let []
-;  
-;  )))
+
+(defn- join-aux
+   "do the natural inner join of the relations from xrels and store data under xrelkeys (defaults to :s0, :s1, ...)"
+  [xrels matchKeys xrelKeys & opts ]
+  {:pre [(sequential? xrels) (sequential? matchKeys) (or (nil? xrelKeys) (sequential? xrelKeys))]}
+  (let [lpf "(vRelation/join-aux): "]
+    (when  (< (count xrels) 2)
+      (vExcept/throw-except lpf "A join requires at least two input relations."))
+     (let [opts (into {:join-type :inner} (apply hash-map opts))
+         ;  _ (println lpf " run with options: " opts)
+           xrelKeys (if (seq xrelKeys) 
+                        xrelKeys
+                        (map #(keyword (str "s" %)) (range))) ;; get defaults
+           spRecs (map #(split-recs %1 matchKeys %2) xrels xrelKeys)
+           get-set-difference (fn [outerSet inner]
+                                 (let [missingKeys (set/difference (set/project outerSet matchKeys)
+                                                     (set/project inner matchKeys))]
+                                   ;(println "missingKeys=" missingKeys)
+                                   ;(println " select-keys-test=" (select-keys (first outerSet) matchKeys))
+                                   (set/select #(missingKeys (select-keys % matchKeys)) (set outerSet))))
+           get-inner-join (fn [spRecs] (reduce set/join spRecs))
+           get-outer-join (fn [spRecs joinType]
+                            (let [outer-join  (fn [cumm joinSet]
+                                                (let [inner (set/join cumm joinSet)
+                                                      restore (get-set-difference cumm inner)
+                                                      restore2 (if (= joinType :full)
+                                                                 (get-set-difference joinSet inner)
+                                                                 #{})]
+                                                  ;; restore records that have been left out of left set
+                                                  ;(println " #cumm=" (count cumm) 
+                                                  ;         " #inner=" (count inner)
+                                                  ;         " #restore=" (count restore))
+                                                  (set/union inner restore restore2)))] 
+                              (reduce outer-join spRecs)))
+         ]
+       (case (:join-type opts)
+        :inner  (get-inner-join spRecs)
+        :left-outer (get-outer-join spRecs :left)
+        :right-outer (get-outer-join (reverse spRecs) :left) ;; left join over reverse sequence --> right join
+        :full-outer  (get-outer-join spRecs :full)
+        (vExcept/throw-except lpf "unknown join-type " (:join-type opts)))))) 
+
+
+(defn natural-inner-join 
+   "Do the natural inner join of the relations from xrels and store data 
+   under xrelkeys (defaults to :s0, :s1, ...)"
+  ([xrels matchKeys]
+    (join-aux xrels matchKeys nil :join-type :inner))
+  ([xrels matchKeys xrelKeys]
+    (join-aux xrels matchKeys xrelKeys :join-type :inner)))
+
+(defn left-outer-join 
+   "Do the left-outer join of the relations from xrels and store data 
+   under xrelkeys (defaults to :s0, :s1, ...). 
+   The joins are applied left to right so a left-outer-join of
+   A, B and C is (A left join B) left join C).
+   "
+  ([xrels matchKeys]
+    (join-aux xrels matchKeys nil :join-type :left-outer))
+  ([xrels matchKeys xrelKeys]
+    (join-aux xrels matchKeys xrelKeys :join-type :left-outer)))
+
+(defn right-outer-join 
+   "Do the right-outer join of the relations from xrels and store data 
+   under xrelkeys (defaults to :s0, :s1, ...).
+   The joins are applied right to left so a right-outer-join of
+   A, B and C is (A right join (B right join C)).
+   "
+  ([xrels matchKeys]
+    (join-aux xrels matchKeys nil :join-type :right-outer))
+  ([xrels matchKeys xrelKeys]
+    (join-aux xrels matchKeys xrelKeys :join-type :right-outer)))
+
+(defn full-outer-join 
+   "do the right-outer join of the relations from xrels and store data 
+   under xrelkeys (defaults to :s0, :s1, ...)"
+  ([xrels matchKeys]
+    (join-aux xrels matchKeys nil :join-type :full-outer))
+  ([xrels matchKeys xrelKeys]
+    (join-aux xrels matchKeys xrelKeys :join-type :full-outer)))
 
